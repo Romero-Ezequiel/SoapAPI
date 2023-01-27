e6eba72fa7fc229547bcff67c7c203c51601e53a
Romero Ezequiel <romerorezequiel@gmail.com>
2023-01-27 17:33:00 -0300

    Ejemplo de servicio soap consumido por una api

---
+++ EjemploSoap/EjemploSoap/Controllers/CalcularController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CalculatorReference;
+using EjemploSoap.Services;
+
+namespace EjemploSoap.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Calcular : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Rest(int num1, int num2)
+        {
+            CalculatorRequest calculatorRequest = new CalculatorRequest();
+            int result = calculatorRequest.GetRest(num1, num2);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult Multiplicacion(int num1, int num2)
+        {
+            CalculatorRequest calculatorRequest = new CalculatorRequest();
+            int result = calculatorRequest.GetMultiplicacion(num1, num2);
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        public IActionResult Division(int num1, int num2)
+        {
+            CalculatorRequest calculatorRequest = new CalculatorRequest();
+            int result = calculatorRequest.GetDivision(num1, num2);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Sum(int num1, int num2)
+        {
+            CalculatorRequest calculatorRequest = new CalculatorRequest();
+            int result = calculatorRequest.GetSum(num1, num2);
+            return Ok(result);
+        }
+
+    }
+
+
+}
---
+++ EjemploSoap/EjemploSoap/Services/CalculatorRequest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using CalculatorReference;
+
+namespace EjemploSoap.Services
+{
+    public class CalculatorRequest
+    {
+        public  int GetSum(int num1, int num2)
+        {
+            var binding = new BasicHttpBinding();
+            // Crear una instancia de endpoint
+            var endpoint = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+            // Crear una instancia de cliente
+            var client = new CalculatorSoapClient(binding, endpoint);
+            // Crear una instancia de la clase Add
+            var add = new CalculatorReference.AddRequest();
+            add.intA = num1;
+            add.intB = num2;
+            // Llamar al método Add del servicio y retornar resultado
+            return client.Add(add).AddResult;
+        }
+
+
+        public int GetRest(int num1, int num2)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+            var client = new CalculatorSoapClient(binding, endpoint);
+            var add = new CalculatorReference.SubtractRequest();
+            add.intA = num1;
+            add.intB = num2;
+            return client.Subtract(add).SubtractResult;
+        }
+
+        public int GetMultiplicacion(int num1, int num2)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+            var client = new CalculatorSoapClient(binding, endpoint);
+            var add = new CalculatorReference.MultiplyRequest();
+            add.intA = num1;
+            add.intB = num2;
+            // Llamar al método Add del servicio y retornar resultado
+            return client.Multiply(add).MultiplyResult;
+        }
+
+        public int GetDivision(int num1, int num2)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+            var client = new CalculatorSoapClient(binding, endpoint);
+            var add = new CalculatorReference.DivideRequest();
+            add.intA = num1;
+            add.intB = num2;
+            return client.Divide(add).DivideResult;
+
+        }
+
+
+    }
+
+
+
+
+}
